d12c0ba13d16bb565561cdc9aebc3590a498ac73
company <1181249332@qq.com>
2018-08-24 18:06:15 +0800

    添加

--- Network/src/com/Client.java
+++ Network/src/com/Client.java
@@ -1,5 +1,7 @@
 package com;
 
+import http.HTTP;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
--- Network/src/com/Server.java
+++ Network/src/com/Server.java
@@ -1,16 +1,22 @@
 package com;
 
-import java.io.FileOutputStream;
+import http.HTTP;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStream;
-import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.UUID;
 
 public class Server {
-	
+	/**
+	 * 初始化服务端
+	 * @param port
+	 */
 	public static void initServer(int port){
 		ServerSocket serverSocket = null;
 		Socket socket =null;
@@ -22,18 +28,30 @@ public class Server {
 			socket =  serverSocket.accept();
 			os = socket.getOutputStream();
 			is = socket.getInputStream();
-			PrintStream ps = new PrintStream(os);
-			ps.print(HTTP.Server+":连接成功!"+UUID.randomUUID().toString());
-			ps.close();
-			os.close();
-			socket.close();
+			ObjectInputStream ois = new ObjectInputStream(is);//反序列化
+			//反序列化
+			ObjectOutputStream oos = new ObjectOutputStream(os);
+			//序列化
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+
+	/**
+	 * @param is
+	 * @throws IOException
+	 */
+	public static void HandleStream(InputStream is) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		StringBuffer sb = new StringBuffer();
+		String Line = null;
+		while((Line=br.readLine())!=null){
+			sb.append(Line);
+		}
+	}
 	
 	public static void main(String[] args) {
-		System.out.println(HTTP.PORT);
+		System.out.println(HTTP.Server+":开启监听端口"+HTTP.PORT);
 		initServer(HTTP.PORT);
 	}
 }
